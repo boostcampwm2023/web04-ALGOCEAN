f584d86ea42f882671938bbb9ff61bc6e10005a1
MayOwall <pixartive@gmail.com>
2023-11-14 19:42:31 +0900

    feat: MainNav 컴포넌트 구현
    - MainNav 컴포넌트를 구현했습니다.
    - public 이미지를 추가했습니다. (write icon)

---
+++ FE/src/components/MainNav/MainNav.styles.ts
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+export const MainNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  height: 3.2rem;
+  padding: 0 3rem;
+  border-bottom: 1px solid var(--color-grayscale-100);
+
+  ol {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+
+    li {
+      font-size: 0.9rem;
+      font-weight: 400;
+      color: var(--color-grayscale-200);
+      cursor: pointer;
+    }
+    li[class='selected'],
+    li:hover {
+      text-decoration: underline;
+      color: var(--color-grayscale-black);
+      font-weight: 700;
+      cursor: default;
+    }
+    li:hover {
+      cursor: pointer;
+    }
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    height: 100%;
+    border: none;
+    padding: 0 1.5rem;
+    color: var(--color-grayscale-white);
+    background-color: var(--color-blue-100);
+
+    img {
+      height: 1rem;
+      aspect-ratio: 1/1;
+    }
+
+    span {
+      position: relative;
+      top: -0.1rem;
+    }
+  }
+`;

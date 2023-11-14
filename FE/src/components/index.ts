65e8cba639e6360ae1c43f4e2d41eb40ce411b56
MayOwall <pixartive@gmail.com>
2023-11-14 21:56:03 +0900

    feat: UniqueQuestions 컴포넌트 구현
    - 질문 게시판의 UniqueQuestions 컴포넌트를 구현했습니다. 컴포넌트는 조회수 급상승 질문, 오늘의 질문, 랜덤 질문으로 구성되어 있습니다.
    - 임시 Main 컴포넌트를 구현했습니다. 레이아웃을 보기 위한 임시방편적 요소 입니다.

--- FE/src/components/index.ts
+++ FE/src/components/index.ts
@@ -1,4 +1,5 @@
 import { MainHeader } from './MainHeader/MainHeader';
 import { MainNav } from './MainNav/MainNav';
+import { UniqueQuestions } from './UniqueQuestions/UniqueQuestions';
 
-export { MainHeader, MainNav };
+export { MainHeader, MainNav, UniqueQuestions };

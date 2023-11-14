546ce16b97b896a8d54bc23920989ac6b94fb130
MayOwall <pixartive@gmail.com>
2023-11-14 18:58:44 +0900

    feat: MainHeader 컴포넌트 구현 및 외
    - MainHeader 컴포넌트 레이아웃을 구현했습니다.
    - public 이미지를 추가했습니다. (logo icon, search icon)
    - 절대 경로를 지정했습니다.
    - npm script 명령어에 --open 옵션을 추가했습니다.

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { MainHeader } from './components';
+import '../style/index.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<div>root</div>} />
-        <Route path="/quest" element={<div>quest</div>} />
-      </Routes>
-    </Router>
+    <>
+      <MainHeader />
+      <Router>
+        <Routes>
+          <Route path="/" element={<div>root</div>} />
+          <Route path="/quest" element={<div>quest</div>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 

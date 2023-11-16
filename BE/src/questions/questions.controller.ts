301045a0b7214feb75104b48838fce0af25c56bc
Kim Tae Ho <kok8454@gmail.com>
2023-11-16 20:09:36 +0900

    feat: 단일 질문 삭제 API
    Question Id에 해당하는 질문을 찾고 IsAdopted가 false인 경우에만 question과 User Id를 받아왔습니다.
    
    question이 null인 경우 false를 반환하도록 하여 questions.controller.ts에서 question을 찾지 못했거나 이미 채택되었다고 처리했습니다.
    
    question.User.Id가 userId와 같지 않을 경우 삭제 요청을 한 유저는 작성자가 아니므로 에러를 던져주어 questions.controller.ts에서 FORBIDDEN을 반환하도록 했습니다.
    
    위의 조건에서 걸러지지 않을 경우 삭제 처리를 하고 true를 반환하도록 했습니다.

--- BE/src/questions/questions.controller.ts
+++ BE/src/questions/questions.controller.ts
@@ -1,10 +1,46 @@
-import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { QuestionsService } from './questions.service';
+import { CreateQuestionDto } from './dto/create-question.dto';
 
 @Controller('questions')
 export class QuestionsController {
   constructor(private readonly questionsService: QuestionsService) {}
+
+  // TODO: Use UserGuard to obtain the user ID and associate it with the question
+  @Post()
+  async createQuestion(
+    @Body() createQuestionDto: CreateQuestionDto,
+    @Res() res: Response,
+  ) {
+    try {
+      const questionId = await this.questionsService.createOneQuestion(
+        createQuestionDto,
+        1,
+      );
+
+      return res
+        .status(HttpStatus.CREATED)
+        .json({ message: 'Question created successfully', id: questionId });
+    } catch (error) {
+      if (error.message === 'Failed to create question') {
+        return res
+          .status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .json({ error: 'Internal server error' });
+      }
+
+      return res.status(HttpStatus.BAD_REQUEST).json({ error: 'Bad request' });
+    }
+  }
 
   @Get(':id')
   async readOneQuestion(@Param('id') id: number, @Res() res: Response) {
@@ -23,5 +59,32 @@ export class QuestionsController {
         .status(HttpStatus.INTERNAL_SERVER_ERROR)
         .json({ error: 'Internal server error' });
     }
+  }
+
+  // TODO: use UserGuard to check if user is the owner of the question
+  @Delete(':id')
+  async deleteOneQuestion(@Param('id') id: number, @Res() res: Response) {
+    try {
+      const isDeleted = await this.questionsService.deleteOneQuestion(id, 1);
+
+      if (isDeleted) {
+        return res
+          .status(HttpStatus.OK)
+          .json({ message: 'Question deleted successfully' });
+      } else {
+        return res
+          .status(HttpStatus.NOT_FOUND)
+          .json({ error: 'Question not found or already adopted' });
+      }
+    } catch (error) {
+      if (error.message === 'User is not the owner of the question') {
+        return res
+          .status(HttpStatus.FORBIDDEN)
+          .json({ error: 'User is not the owner of the question' });
+      }
+    }
+    return res
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .json({ error: 'Internal server error' });
   }
 }
--- BE/src/questions/questions.service.ts
+++ BE/src/questions/questions.service.ts
@@ -1,10 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { ReadQuestionDto } from './dto/read-question.dto';
+import { CreateQuestionDto } from './dto/create-question.dto';
 
 @Injectable()
 export class QuestionsService {
   constructor(private prisma: PrismaService) {}
+
+  // TODO: Implement logic to associate the question with the user by including the user id
+  async createOneQuestion(
+    createQuestionDto: CreateQuestionDto,
+    userId: number,
+  ): Promise<number> {
+    try {
+      const question = await this.prisma.question.create({
+        data: {
+          User: {
+            connect: {
+              Id: userId,
+            },
+          },
+          Title: createQuestionDto.title,
+          Content: createQuestionDto.content,
+          Tag: createQuestionDto.tag,
+          ProgrammingLanguage: createQuestionDto.programmingLanguage,
+          OriginalLink: createQuestionDto.originalLink,
+        },
+      });
+      return question.Id;
+    } catch (error) {
+      throw new Error('Failed to create question');
+    }
+  }
 
   async readOneQuestion(id: number): Promise<ReadQuestionDto> {
     const question = await this.prisma.question.findUnique({
@@ -37,5 +64,38 @@ export class QuestionsService {
       likeCount: question.LikeCount,
       isLiked: question.User.LikeInfo[0]?.IsLiked || false,
     };
+  }
+
+  // TODO: check if user is owner of the question
+  async deleteOneQuestion(id: number, userId: number): Promise<boolean> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        Id: id,
+        IsAdopted: false,
+      },
+      include: {
+        User: {
+          select: {
+            Id: true,
+          },
+        },
+      },
+    });
+
+    if (!question) {
+      return false;
+    }
+
+    if (question.User.Id !== userId) {
+      throw new Error('User is not the owner of the question');
+    }
+
+    await this.prisma.question.delete({
+      where: {
+        Id: id,
+      },
+    });
+
+    return true;
   }
 }

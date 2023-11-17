b9f1aa36b118e51edf029e7de29859ffef65c63f
perArdua <70432496+perArdua@users.noreply.github.com>
2023-11-18 01:54:59 +0900

    단일 질문 생성 API (#60)
    * feat: 단일 질문 생성 API
    
    create-question-list.dto.ts를 구성하기 위해 필요한 값들을 controller에서 받고 이를 questions.service.ts에 넘겨주어 질문을 생성했습니다.
    
    User의 정보 중 UserId는 1이라는 값으로 설정했습니다. 추후에 User의 정보를 받아오는 기능을 작성하면 이를 토대로 수정해야합니다.
    
    * fix: Remove unnecessary UsePipes
    
    테스트 용으로 UsePipes코드를 사용했는데 이를 지우지 않은 채 푸시했습니다. 이는 전역으로 설정된 코드이기 때문에 불필요한 코드라고 판단되어 삭제했습니다.
    
    * fix: 예외처리 상황 추가
    
    기존 코드는 200상태가 아니면 404상태를 반환하도록 했는데, createOneQuestion이 실패하는 상황을 고려하지 않았습니다.
    createOneQuestion이 실패하는 상황을 고려하기 위해 500상태 반환을 추가했습니다.
    
    * fix: 누락된 dto파일 추가
    
    누락된 create-question.dto.ts 파일을 추가했습니다.
    
    * fix: 완료된 TODO 삭제
    
    완료된 기능을 TODO에 적어두었기 때문에 삭제했습니다.

--- BE/src/questions/questions.controller.ts
+++ BE/src/questions/questions.controller.ts
@@ -1,11 +1,47 @@
-import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { QuestionsService } from './questions.service';
+import { CreateQuestionDto } from './dto/create-question.dto';
 import { ReadQuestionListDto } from './dto/read-question-list.dto';
 
 @Controller('questions')
 export class QuestionsController {
   constructor(private readonly questionsService: QuestionsService) {}
+
+  // TODO: Use UserGuard to obtain the user ID and associate it with the question
+  @Post()
+  async createQuestion(
+    @Body() createQuestionDto: CreateQuestionDto,
+    @Res() res: Response,
+  ) {
+    try {
+      const questionId = await this.questionsService.createOneQuestion(
+        createQuestionDto,
+        1,
+      );
+
+      return res
+        .status(HttpStatus.CREATED)
+        .json({ message: 'Question created successfully', id: questionId });
+    } catch (error) {
+      if (error.message === 'Failed to create question') {
+        return res
+          .status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .json({ error: 'Internal server error' });
+      }
+
+      return res.status(HttpStatus.BAD_REQUEST).json({ error: 'Bad request' });
+    }
+  }
 
   @Get(':id')
   async readOneQuestion(@Param('id') id: number, @Res() res: Response) {

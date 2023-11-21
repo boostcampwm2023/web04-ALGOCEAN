ffa85afb44ac621f9c2ea7de4d3372a9bc0f1b12
Seunggyu Lee <dltmdrb98@gmail.com>
2023-11-22 00:56:55 +0900

    test: 아이디 중복 확인 API 테스트 코드 작성
    아이디 중복 확인 API controller, service에 대한 테스트 코드 작성

--- BE/src/users/users.controller.spec.ts
+++ BE/src/users/users.controller.spec.ts
@@ -1,20 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [UsersService, PrismaService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('checkUserId', () => {
+    it('should return { isTaken: true } if user ID is taken', async () => {
+      const existingUserId = 'existingUser';
+      jest
+        .spyOn(service, 'isUserIdTaken')
+        .mockImplementation(async (userId) => userId === existingUserId);
+
+      const result = await controller.checkUserId(existingUserId);
+      expect(result).toEqual({ isTaken: true });
+    });
+
+    it('should return { isTaken: false } if user ID is not taken', async () => {
+      const nonExistingUserId = 'nonExistingUser';
+      jest
+        .spyOn(service, 'isUserIdTaken')
+        .mockImplementation(async (userId) => userId !== nonExistingUserId);
+
+      const result = await controller.checkUserId(nonExistingUserId);
+      expect(result).toEqual({ isTaken: false });
+    });
   });
 });
--- BE/src/users/users.service.spec.ts
+++ BE/src/users/users.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, PrismaService],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -14,5 +15,29 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('isUserIdTaken', () => {
+    it('should return true if user ID is taken', async () => {
+      const existingUserId = 'existingUser';
+
+      jest
+        .spyOn(service, 'isUserIdTaken')
+        .mockImplementation(async (userId) => userId === existingUserId);
+
+      const result = await service.isUserIdTaken(existingUserId);
+      expect(result).toBe(true);
+    });
+
+    it('should return false if user ID is not taken', async () => {
+      const nonExistingUserId = 'nonExistingUser';
+
+      jest
+        .spyOn(service, 'isUserIdTaken')
+        .mockImplementation(async (userId) => userId !== nonExistingUserId);
+
+      const result = await service.isUserIdTaken(nonExistingUserId);
+      expect(result).toBe(false);
+    });
   });
 });

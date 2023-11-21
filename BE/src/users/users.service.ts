674aed1af7af390ea55a6efd885aa8eca19805ef
Seunggyu Lee <dltmdrb98@gmail.com>
2023-11-22 00:36:15 +0900

    feat: 아이디 중복 확인 API
    /api/users/check-username/:userId
    로 요청을 보내면
    {
    "isTaken" : ture
    }
    이런식의
    응답을 보낸다.
    중복되는 유저 아이디가 이미 존재한다면 true, 없다면 false를 보낸다.

--- BE/src/users/users.service.ts
+++ BE/src/users/users.service.ts
@@ -1,26 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(private prisma: PrismaService) {}
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async isUserIdTaken(userId: string): Promise<boolean> {
+    const user = await this.prisma.user.findUnique({
+      where: { UserId: userId },
+    });
+    return !!user;
   }
 }

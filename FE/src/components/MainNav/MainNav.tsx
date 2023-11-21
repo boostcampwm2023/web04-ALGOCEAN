be4922bd9fa30fdd9579ba31b77807efbc648377
이지은 (오월) <pixartive@gmail.com>
2023-11-21 14:07:51 +0900

    페이지네이션 컴포넌트 구현 및 질문 리스트 컴포넌트에 적용 (#57)
    * feat: Pagination 컴포넌트 기능 구현
    
    - Pagination 컴포넌트 기능을 구현했습니다.
    - 질문 리스트 페이지네이션 기능을 적용했습니다.
    
    * feat: 전역, MainHeader, MainNav CSS 변경
    
    CSS 변경을 통해 브라우저 화면 축소해도 과도한 화면 늘어남이 없도록 구현했습니다.

--- FE/src/components/MainNav/MainNav.tsx
+++ FE/src/components/MainNav/MainNav.tsx
@@ -12,21 +12,23 @@ export function MainNav() {
 
   return (
     <S.MainNav>
-      <ol>
-        <li className={currentNavItem === 'question' ? 'selected' : ''}>
-          질문 게시판
-        </li>
-        <li className={currentNavItem === 'ranking' ? 'selected' : ''}>
-          랭킹 게시판
-        </li>
-        <li className={currentNavItem === 'point' ? 'selected' : ''}>
-          포인트 상점
-        </li>
-      </ol>
-      <button>
-        <img src={writeIcon} alt="질문하기" />
-        <span>질문하기</span>
-      </button>
+      <div className="inner">
+        <ol>
+          <li className={currentNavItem === 'question' ? 'selected' : ''}>
+            질문 게시판
+          </li>
+          <li className={currentNavItem === 'ranking' ? 'selected' : ''}>
+            랭킹 게시판
+          </li>
+          <li className={currentNavItem === 'point' ? 'selected' : ''}>
+            포인트 상점
+          </li>
+        </ol>
+        <button>
+          <img src={writeIcon} alt="질문하기" />
+          <span>질문하기</span>
+        </button>
+      </div>
     </S.MainNav>
   );
 }
--- FE/src/components/QuestionList/QuestionList.styles.ts
+++ FE/src/components/QuestionList/QuestionList.styles.ts
@@ -49,6 +49,7 @@ export const Item = styled.li<ItemProps>`
       justify-content: left;
       gap: 0.2rem;
       width: 100%;
+      height: 1.3rem;
 
       h4 {
         flex-shrink: 1;

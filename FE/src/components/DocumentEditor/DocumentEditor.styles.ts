f378a58e6b383e5ad5e1a98bc97ea42122669557
BaeJ1H021 <blueapple99@yonsei.ac.kr>
2023-11-22 19:54:04 +0900

    feat: 문서 에디터 구현
    - react-draft-wysiwyg, draft-js를 사용하여 구현
    - draftjs-to-html을 사용하여 html형식으로 서버에 보낼 예정
    - 이미지 업로드 기능 추후에 추가할 예정

---
+++ FE/src/components/DocumentEditor/DocumentEditor.styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const EditorWrapper = styled.div`
+  .wrapper-class {
+    width: 85%;
+    margin: 0 auto;
+    margin-bottom: 4rem;
+    background-color: white;
+  }
+  .editor {
+    height: 31.25rem !important;
+    border: 1px solid #f1f1f1 !important;
+    padding: 5px !important;
+    border-radius: 2px !important;
+  }
+  .toolbar-class {
+  }
+`;
---
+++ FE/src/components/DocumentEditor/DocumentEditor.tsx
@@ -0,0 +1,44 @@
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import { useState } from 'react';
+import { EditorWrapper } from './DocumentEditor.styles';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
+const DocumentEditor = () => {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const onEditorStateChange = (newEditorState: EditorState) => {
+    setEditorState(newEditorState);
+  };
+
+  return (
+    <EditorWrapper>
+      <Editor
+        // 에디터와 툴바 모두에 적용되는 클래스
+        wrapperClassName="wrapper-class"
+        // 에디터에만 적용되는 클래스
+        editorClassName="editor"
+        // 툴바에 적용되는 클래스
+        toolbarClassName="toolbar-class"
+        // 툴바 설정
+        toolbar={{
+          list: { inDropdown: true },
+          textAlign: { inDropdown: true },
+          link: { inDropdown: true },
+          history: { inDropdown: false },
+        }}
+        placeholder="내용을 입력해주세요."
+        // 언어 설정
+        localization={{
+          locale: 'ko',
+        }}
+        // 에디터에서 어떤 상태를 다룰 지 설정
+        editorState={editorState}
+        // 에디터 값 변경 감지
+        onEditorStateChange={onEditorStateChange}
+      />
+    </EditorWrapper>
+  );
+};
+
+export default DocumentEditor;

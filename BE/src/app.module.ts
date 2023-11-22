984518487a064d7b3c8c3a9d69ad46e059405611
Seunggyu Lee <dltmdrb98@gmail.com>
2023-11-22 21:28:21 +0900

    feat: auth 모듈 생성
    nest g resource auth 후 불필요한 코드 제거

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AnswersModule } from './answers/answers.module';
 import { QuestionsModule } from './questions/questions.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AnswersModule, QuestionsModule, UsersModule],
+  imports: [AnswersModule, QuestionsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })

c0483244347b20bc72a6fb4b0054524c2d9dc528
Seunggyu Lee <dltmdrb98@gmail.com>
2023-11-22 11:13:49 +0900

    아이디 중복 확인 api (#89)
    * feat: users 모듈 생성
    
    nest g resource users로 users 모듈 생성
    prisma를 사용하므로 entity 디렉토리는 삭제
    
    * fix: db date에 default 값 적용
    
    Answer의 CreatedAt, UpdatedAt
    Point_History의 ChangeDate
    Question의 CreatedAt, UpdatedAt
    User의 CreatedAt, UpdatedAt
    
    default값을 생성시간으로 설정
    
    * feat: 아이디 중복 확인 API
    
    /api/users/check-username/:userId
    로 요청을 보내면
    {
    "isTaken" : ture
    }
    이런식의
    응답을 보낸다.
    중복되는 유저 아이디가 이미 존재한다면 true, 없다면 false를 보낸다.
    
    * test: 아이디 중복 확인 API 테스트 코드 작성
    
    아이디 중복 확인 API controller, service에 대한 테스트 코드 작성

---
+++ BE/src/users/users.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async isUserIdTaken(userId: string): Promise<boolean> {
+    const user = await this.prisma.user.findUnique({
+      where: { UserId: userId },
+    });
+    return !!user;
+  }
+}

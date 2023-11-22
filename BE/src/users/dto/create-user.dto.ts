2365eadc4a1141dd367397c7b0d6e834aced4ed7
Seunggyu Lee <dltmdrb98@gmail.com>
2023-11-22 18:00:22 +0900

    회원 가입 API (#90)
    * feat: users 모듈 생성
    
    nest g resource users로 users 모듈 생성
    prisma를 사용하므로 entity 디렉토리는 삭제
    
    * fix: db date에 default 값 적용
    
    Answer의 CreatedAt, UpdatedAt
    Point_History의 ChangeDate
    Question의 CreatedAt, UpdatedAt
    User의 CreatedAt, UpdatedAt
    
    default값을 생성시간으로 설정
    
    * feat: 아이디 중복 확인 API
    
    /api/users/check-username/:userId
    로 요청을 보내면
    {
    "isTaken" : ture
    }
    이런식의
    응답을 보낸다.
    중복되는 유저 아이디가 이미 존재한다면 true, 없다면 false를 보낸다.
    
    * test: 아이디 중복 확인 API 테스트 코드 작성
    
    아이디 중복 확인 API controller, service에 대한 테스트 코드 작성
    
    * build: 암호화 의존성 bcrypt 설치
    
    bcrypt, dev 의존성 @types/bcrypt 설치
    
    * feat: 회원가입 API
    
    uesrId, password, nickname을 통해 회원가입을 할 수 있는 API 제공
    
    profileImage도 바로 등록할 수 있지만 꼭 안 넣어도 회원가입에 문제는 없다.
    
    error 처리를 어디서 하는지에 대한 대한 고민이 있다.
    
    * fix: 아이디 중복 확인 API 요청에 profileImage 제거
    
    profileImage를 요청에 포함하지 않으면 400을 반환하는 문제가 있어서 수정
    
    * refactor: 사용하지 않는 반환값 제거
    
    controller에서 service의 반환값을 사용하지 않아서 제거했다.

--- BE/src/users/dto/create-user.dto.ts
+++ BE/src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly userId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly nickname: string;
+}

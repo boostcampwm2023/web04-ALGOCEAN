bc2bf18a7ffef45cf2c37de551a468cc57cf5f05
MayOwall <pixartive@gmail.com>
2023-11-22 18:20:44 +0900

    feat: 질문리스트 GET API 구현
    - axios 설치
    - Server Base url 환경변수로 설정
    - 페이지에 따른 질문리스트 GET API 구현

---
+++ FE/src/api/index.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+const { VITE_BASE_URL } = import.meta.env;
+
+export const getQuestionList = async (page: Page) => {
+  try {
+    const url = `${VITE_BASE_URL}/api/questions/lists/${page}`;
+    const { status, data } = await axios.get(url);
+    if (status !== 200) {
+      throw new Error();
+    }
+    return data;
+  } catch (e) {
+    console.error(e);
+  }
+};

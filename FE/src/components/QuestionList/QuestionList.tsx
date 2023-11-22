a55b00105d19dda808640ad6a5f30d6b5f832e12
이지은 (오월) <pixartive@gmail.com>
2023-11-23 03:03:40 +0900

    메인페이지 질문리스트 : 채택받은 질문 디자인 적용 (#96)
    * chore: svgr 플러그인 설치 및 환경 설정
    
    - vite-react 환경에서 svg 파일을 컴포넌트처럼 사용할 수 있도록 해주는 vite-plugin-svgr을 설치했습니다.
    - vite 플러그인 목록에 vite-plugin-svgr을 추가해 주었습니다.
    - svgr 플러그인 관련 타입을 선언해주었습니다.
    
    * feat: custom hook - getFormalizedDate 구현
    
    - date string을 넣으면 0000.00.00 형태의 string으로 리턴해주는 커스텀 훅 제작
    
    * feat: 질문리스트 - 채택 받은 질문 디자인 적용
    
    - 채택 받은 질문 상태에 따른 디자인 적용
    - 질문 리스트 아이템 스타일 변경
    - 기존의 일반 jsx 태그를 styled-component 태그로 적극 변경
    - 더 이상 사용하지 않는 메서드 (getColor) 제거
    - 현재 사용하지 않는 <Header/> 컴포넌트 제거
    
    * refactor: 잘못된 렌더링 로직 수정
    
    - 채택 받은 질문 랜더링 로직이 반대로 되어있어 수정했습니다.
    - 질문 리스트 아이템 CSS 수정
    
    * refactor: 코드 리뷰 사항 반영
    
    - styled-component import 방식 변경

--- FE/src/components/QuestionList/QuestionList.tsx
+++ FE/src/components/QuestionList/QuestionList.tsx
@@ -3,7 +3,26 @@ import { Pagination } from '../index';
 import { getQuestionList } from '../../api';
 import eyeIcon from '/icons/eye.svg';
 import likeIcon from '/icons/like.svg';
-import * as S from './QuestionList.styles';
+import CheckIcon from '../../assets/icons/check-circle.svg?react';
+import { getFormalizedDate } from '../../hooks/index';
+import {
+  Header as HeaderContainer,
+  Item as ItemContainer,
+  ItemMain,
+  Title,
+  Details,
+  AdoptBadge,
+  Author,
+  ItemAside,
+  Tag,
+  ProgrammingLanguage,
+  ViewCount,
+  LikeCount,
+  QuestionList as QuestionListContainer,
+} from './QuestionList.styles';
+
+const LAST_PAGINATION_PAGE = 11;
+const PAGINATION_SPLIT_NUMBER = 10;
 
 const LAST_PAGINATION_PAGE = 11;
 const PAGINATION_SPLIT_NUMBER = 10;
@@ -20,23 +39,12 @@ interface ItemData {
   likeCount: number;
 }
 
-const getColor = (programmingLanguage: string) => {
-  switch (programmingLanguage) {
-    case '프로그래머스':
-      return 'var(--color-programmers)';
-    case '리트코드':
-      return 'var(--color-leetcode)';
-    default:
-      return 'var(--color-baekjoon)';
-  }
-};
-
 export function Header() {
   return (
-    <S.Header>
+    <HeaderContainer>
       <div className="by-recent selected">✔️ 최신순</div>
       <div className="by-old">오래된순</div>
-    </S.Header>
+    </HeaderContainer>
   );
 }
 
@@ -54,30 +62,33 @@ export function Item({ itemData }: { itemData: ItemData }) {
   } = itemData;
 
   return (
-    <S.Item data-id={id} color={getColor(tag)}>
-      <div className="main">
-        <div className="title">
-          <h4>{title}</h4>
-          {!!isAdopted && <div className="adopted">채택 완료</div>}
-        </div>
-        <div className="details">
-          <span className="tag">{tag}</span>
-          <span className="programming-language">{programmingLanguage}</span>
-          <span className="nickname">{nickname}</span>
-          <span className="created-at">{createdAt}</span>
-        </div>
-      </div>
-      <div className="aside">
-        <div className="view-count">
+    <ItemContainer data-id={id}>
+      <ItemMain>
+        <Title>{title}</Title>
+        <Details>
+          {isAdopted && (
+            <AdoptBadge>
+              <CheckIcon />
+              채택 완료
+            </AdoptBadge>
+          )}
+          <Author>{nickname}</Author>
+          <Date>{getFormalizedDate(createdAt)}</Date>
+        </Details>
+      </ItemMain>
+      <ItemAside>
+        <Tag tag={tag}>{tag}</Tag>
+        <ProgrammingLanguage>{programmingLanguage}</ProgrammingLanguage>
+        <ViewCount>
           <img src={eyeIcon} />
           <span>{viewCount}</span>
-        </div>
-        <div className="like-count">
+        </ViewCount>
+        <LikeCount>
           <img src={likeIcon} alt="좋아요 수" />
           <span>{likeCount}</span>
-        </div>
-      </div>
-    </S.Item>
+        </LikeCount>
+      </ItemAside>
+    </ItemContainer>
   );
 }
 
@@ -101,8 +112,7 @@ export function QuestionList() {
   }, [currentPage]);
 
   return (
-    <S.QuestionList>
-      <Header />
+    <QuestionListContainer>
       <ul>
         {!!questionListData &&
           questionListData.map((itemData, idx) => (
@@ -115,6 +125,6 @@ export function QuestionList() {
         handleCurrentPage={handleCurrentPage}
         splitNumber={PAGINATION_SPLIT_NUMBER}
       />
-    </S.QuestionList>
+    </QuestionListContainer>
   );
 }

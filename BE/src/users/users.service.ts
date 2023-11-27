64d44063551ea18ed35865cc83acfa18f231ba13
Seunggyu Lee <dltmdrb98@gmail.com>
2023-11-27 23:02:55 +0900

    fix: 예외 처리, 삭제된 유저 고려
    찾는 유저가 없는 경우 예외 처리
    삭제된 유저 제외

--- BE/src/users/users.service.ts
+++ BE/src/users/users.service.ts
@@ -45,10 +45,16 @@ export class UsersService {
   }
 
   async getUserByUserId(userId: string) {
-    return this.prisma.user.findUnique({
-      where: { UserId: userId },
+    const user = await this.prisma.user.findUnique({
+      where: { UserId: userId, DeletedAt: null },
       select: { Nickname: true },
     });
+
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+    }
+
+    return user;
   }
 
   async getUserGrade(userId: string): Promise<string> {

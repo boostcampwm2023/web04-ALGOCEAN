669702eeb48504a1ade07a3b6baf173b130c96f5
Kim Tae Ho <kok8454@gmail.com>
2023-11-27 18:36:32 +0900

    fix: 좋아요 모듈 수정
    provider에 PrismaService를 추가했습니다

--- BE/src/likes/likes.module.ts
+++ BE/src/likes/likes.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LikesController } from './likes.controller';
 import { LikesService } from './likes.service';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [LikesController],
-  providers: [LikesService]
+  providers: [LikesService, PrismaService],
 })
 export class LikesModule {}

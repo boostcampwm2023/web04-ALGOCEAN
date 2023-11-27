760458f3c1f6c950b7dc69cbfcca1ca2ff383980
Kim Tae Ho <kok8454@gmail.com>
2023-11-27 17:41:09 +0900

    feat: 좋아요 모듈 추가

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AnswersModule } from './answers/answers.module';
 import { QuestionsModule } from './questions/questions.module';
 import { UsersModule } from './users/users.module';
+import { LikesModule } from './likes/likes.module';
 
 @Module({
-  imports: [AnswersModule, QuestionsModule, UsersModule],
+  imports: [AnswersModule, QuestionsModule, UsersModule, LikesModule],
   controllers: [AppController],
   providers: [AppService],
 })

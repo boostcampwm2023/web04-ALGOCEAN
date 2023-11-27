f1b884a5ccba73aa3aaa398eb50600f314e6eb48
Kim Tae Ho <kok8454@gmail.com>
2023-11-27 18:35:57 +0900

    refactor: 좋아요 API
    기존 코드와 수행하는 동작은 동일합니다.
    기존 코드의 가독성이 좋지 않아 변경했습니다.

--- BE/src/likes/likes.service.ts
+++ BE/src/likes/likes.service.ts
@@ -5,111 +5,61 @@ import { PrismaService } from '../prisma.service';
 @Injectable()
 export class LikesService {
   constructor(private readonly prisma: PrismaService) {}
+
+  private async createLikeInfo(
+    id: number,
+    userId: number,
+    type: string,
+    isLiked: boolean,
+  ) {
+    return this.prisma.likeInfo.create({
+      data: {
+        UserId: userId,
+        LikedEntityId: id,
+        LikedEntityType: type,
+        IsLiked: isLiked,
+      },
+    });
+  }
+
+  private async updateLikeInfo(id: number, isLiked: boolean, type: string) {
+    return this.prisma[type].update({
+      where: { Id: id },
+      data: {
+        LikeCount: isLiked ? { increment: 1 } : { decrement: 1 },
+      },
+    });
+  }
+
   async toggleLike(
     userId: number,
     createLikesDto: CreateLikesDto,
   ): Promise<number> {
     const { id, type } = createLikesDto;
 
-    const existingLike = await this.prisma.likeInfo.findFirst({
-      where: {
-        UserId: userId,
-        LikedEntityId: id,
-      },
-    });
-
-    if (existingLike) {
-      if (existingLike.IsLiked) {
-        const likeUpdate = this.prisma.likeInfo.update({
-          where: {
-            Id: existingLike.Id,
-          },
-          data: {
-            IsLiked: false,
-          },
-        });
-
-        const likeCountUpdate = this.decreaseLikeCount(type, id);
-        await this.prisma.$transaction([likeUpdate, likeCountUpdate]);
-        return 0;
-      } else {
-        const likeUpdate = this.prisma.likeInfo.update({
-          where: {
-            Id: existingLike.Id,
-          },
-          data: {
-            IsLiked: true,
-          },
-        });
-
-        const likeCountUpdate = this.increaseLikeCount(type, id);
-        await this.prisma.$transaction([likeUpdate, likeCountUpdate]);
-        return 1;
-      }
-    } else {
-      const likeUpdate = await this.prisma.likeInfo.create({
-        data: {
+    return this.prisma.$transaction(async (prisma) => {
+      const existingLike = await prisma.likeInfo.findFirst({
+        where: {
           UserId: userId,
           LikedEntityId: id,
-          LikedEntityType: type,
-          IsLiked: true,
         },
       });
-      const likeCountUpdate = this.increaseLikeCount(type, id);
-      await this.prisma.$transaction([likeUpdate, likeCountUpdate]);
-      return 1;
-    }
-  }
 
-  async increaseLikeCount(type: string, id: number) {
-    if (type === 'question') {
-      return this.prisma.question.update({
-        where: {
-          Id: id,
-        },
-        data: {
-          LikeCount: {
-            increment: 1,
-          },
-        },
-      });
-    } else {
-      return this.prisma.answer.update({
-        where: {
-          Id: id,
-        },
-        data: {
-          LikeCount: {
-            increment: 1,
-          },
-        },
-      });
-    }
-  }
+      if (existingLike) {
+        const isLiked = !existingLike.IsLiked;
 
-  async decreaseLikeCount(type: string, id: number) {
-    if (type === 'question') {
-      return this.prisma.question.update({
-        where: {
-          Id: id,
-        },
-        data: {
-          LikeCount: {
-            decrement: 1,
-          },
-        },
-      });
-    } else {
-      return this.prisma.answer.update({
-        where: {
-          Id: id,
-        },
-        data: {
-          LikeCount: {
-            decrement: 1,
-          },
-        },
-      });
-    }
+        await prisma.likeInfo.update({
+          where: { Id: existingLike.Id },
+          data: { IsLiked: isLiked },
+        });
+
+        await this.updateLikeInfo(id, isLiked, type);
+        return isLiked ? 1 : 0;
+      } else {
+        await this.createLikeInfo(id, userId, type, true);
+        await this.updateLikeInfo(id, true, type);
+        return 1;
+      }
+    });
   }
 }

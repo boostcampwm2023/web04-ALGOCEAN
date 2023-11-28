cc297c382a3653675e695c49b5b7aabeb6af9076
Seunggyu Lee <dltmdrb98@gmail.com>
2023-11-29 00:40:02 +0900

    feat: 질문에 대한 답변 리스트 API
    질문에 대한 답변 리스트 API 구현
    로그인이 되어있는 사용자의 경우 좋아요를 눌렀는지 안 눌렀는지도 알려줘야하지만 아직 구현하지
    못했음.

--- BE/src/answers/answers.service.ts
+++ BE/src/answers/answers.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateAnswerDto } from './dto/create-answer.dto';
 import { PrismaService } from '../prisma.service';
 import { AdoptAnswerDto } from './dto/adopt-answer.dto';
@@ -15,6 +15,31 @@ export class AnswersService {
         UserId: 1, // TODO: get user id from request
       },
     });
+  }
+
+  async findAllByQuestionId(questionId: string) {
+    try {
+      const questionIdAsNumber = parseInt(questionId, 10);
+      return this.prisma.answer.findMany({
+        where: { QuestionId: questionIdAsNumber, DeletedAt: null },
+        select: {
+          Id: true,
+          User: {
+            select: {
+              Id: true,
+              Nickname: true,
+              ProfileImage: true,
+            },
+          },
+          Content: true,
+          VideoLink: true,
+          IsAdopted: true,
+          CreatedAt: true,
+        },
+      });
+    } catch (e) {
+      throw new HttpException('Answers not found', 404);
+    }
   }
 
   async adopt(adoptAnswerDto: AdoptAnswerDto) {

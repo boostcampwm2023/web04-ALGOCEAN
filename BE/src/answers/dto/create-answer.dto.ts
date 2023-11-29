a97a69b780c214f3329b7dc4ad479ed641035f34
Kim Tae Ho <kok8454@gmail.com>
2023-11-29 10:59:37 +0900

    feat: dto에 api property를 작성
    ApiProperty가 없는 dto들에게 ApiProperty를 달아주었습니다.

--- BE/src/answers/dto/create-answer.dto.ts
+++ BE/src/answers/dto/create-answer.dto.ts
@@ -1,9 +1,20 @@
 import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateAnswerDto {
+  @ApiProperty({
+    example: '이것은 답변입니다',
+    description: '답변 내용',
+    type: 'string',
+  })
   @IsNotEmpty()
   @IsString()
   content: string;
 
+  @ApiProperty({
+    example: 'https://www.youtube.com',
+    description: '답변에 첨부할 동영상 링크',
+    type: 'string',
+  })
   @IsNotEmpty()
   @IsUrl()
   videoLink: string;

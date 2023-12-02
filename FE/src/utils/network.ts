968a6bdc5c9240ec13cd24a7e4e8a9fe5989481e
BaeJ1H021 <blueapple99@yonsei.ac.kr>
2023-12-02 18:10:10 +0900

    feat: S3 인스턴스 생성
    - NCP에 파일을 업로드하기 위한 AWS S3 인스턴스 추가

--- FE/src/utils/network.ts
+++ FE/src/utils/network.ts
@@ -1,6 +1,21 @@
+import AWS from 'aws-sdk';
 import axios from 'axios';
-const { VITE_BASE_URL } = import.meta.env;
+
+const { VITE_BASE_URL, VITE_AWS_ACCESS_KEY_ID, VITE_AWS_SECRET_ACCESS_KEY } =
+  import.meta.env;
+// NCP
+const endpoint = 'kr.object.ncloudstorage.com';
+const region = 'kr-standard';
 
 export const client = axios.create({
   baseURL: VITE_BASE_URL,
+});
+
+export const S3 = new AWS.S3({
+  endpoint: endpoint,
+  region: region,
+  credentials: {
+    accessKeyId: VITE_AWS_ACCESS_KEY_ID,
+    secretAccessKey: VITE_AWS_SECRET_ACCESS_KEY,
+  },
 });

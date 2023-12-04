2ebc05fabcf7cc1ac682ef4044905f73e26a5dd2
Kim Tae Ho <kok8454@gmail.com>
2023-12-04 17:27:01 +0900

    feat: UsersModule에 의존성 주입
    QuestionsService, AnswersService를 주입했습니다.

--- BE/src/users/users.module.ts
+++ BE/src/users/users.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from '../prisma.service';
+import { QuestionsService } from '../questions/questions.service';
+import { AnswersService } from '../answers/answers.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService, PrismaService, QuestionsService, AnswersService],
   exports: [UsersService],
 })
 export class UsersModule {}

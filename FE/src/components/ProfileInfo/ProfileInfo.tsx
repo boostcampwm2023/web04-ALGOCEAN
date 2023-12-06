2bf6d20ad263c70f3a0e98132aea0488e09e31e6
BaeJ1H021 <87417773+BaeJ1H021@users.noreply.github.com>
2023-12-06 13:18:32 +0900

    프로필 페이지 api 연동
    * feat: 프로필 인포 컴포넌트 레이아웃 구현
    
    - 프로필 인포 컴포넌트 추가
    
    * feat: 프로필 개인 질문리스트 레이아웃 구현
    
    - 프로필 개인 질문리스트 컴포넌트 추가
    
    * feat: html 형식 텍스트 -> 일반 text 변환하는 함수 추가
    
    - html태그 제거해주는 함수 구현
    
    * feat: 프로필 개인 답변리스트 레이아웃 구현
    
    - 프로필 개인 답변리스트 컴포넌트 추가
    
    * feat: 프로필 페이지 레이아웃 구현
    
    - 프로필 페이지 추가
    
    * feat: 질문, 답변리스트 타입 선언
    
    - 질문, 답변리스트 타입 추가
    
    * feat: 프로필 페이지 api 구현
    
    - 유저 포인트 조회 api 추가
    - 유저 닉네임 조회 api 추가
    - 유저 등급 조회 api 추가
    - 유저 질문리스트 조회 api 추가
    - 유저 답변리스트 조회 api 추가
    
    * feat: 프로필 페이지 api 연동
    
    - 프로필 페이지에서 자식 컴포넌트로 fetching한 data props로 전달
    
    ---------
    
    Co-authored-by: 이지은 (오월) <pixartive@gmail.com>

--- FE/src/components/ProfileInfo/ProfileInfo.tsx
+++ FE/src/components/ProfileInfo/ProfileInfo.tsx
@@ -12,15 +12,29 @@ import {
 import Programmer from '/images/Programmer.png';
 
 const dummyTopic = ['등수', '포인트', '전체질문', '전체답변', '채택답변'];
-const dummyContent = ['1817', '543', '13건', '6건', '3건'];
+const dummyContent = ['1817', 0, '13건', '6건', '3건'];
 
-export function ProfileInfo() {
+interface ProfileInfoProps {
+  userPoint: number;
+  userNickname: string;
+  userGrade: string;
+}
+
+export function ProfileInfo({
+  userPoint,
+  userNickname,
+  userGrade,
+}: ProfileInfoProps) {
+  // dummyContent에 넣은 이유는 아직 등수, 전체질문, 전체답변, 채택답변 등에 대한 api가 없어서다.
+  // 현상태로 Content 태그에 userPoint를 직접 넣는 것은 쓸모없다고 판단하였다.
+  // 추후에 api가 개발되면 dummyContent를 없앨 예정이다.
+  dummyContent[1] = userPoint;
   return (
     <Section>
       <PersonalInfoLeftDiv>
         <ProfileImage src={Programmer} />
-        <Nickname>겨울엔역시고구마</Nickname>
-        <Rank>Bronze</Rank>
+        <Nickname>{userNickname}</Nickname>
+        <Rank>{userGrade}</Rank>
       </PersonalInfoLeftDiv>
       <PersonalInfoRightDiv>
         {dummyTopic.map((topic, idx) => (
--- FE/src/components/ProfileQuestionList/ProfileQuestionList.tsx
+++ FE/src/components/ProfileQuestionList/ProfileQuestionList.tsx
@@ -6,42 +6,15 @@ import {
   Topic,
 } from './ProfileQuestionList.styles';
 import { getFormalizedDate } from '../../hooks';
+import { QuestionList } from 'src/types/type';
 
 const headerList = ['제목', '태그', '언어', '채택', '좋아요', '작성일'];
-const dummyData = [
-  {
-    Id: 73762,
-    Title: '다익스트라 알고리즘의 정당성이 궁금해요',
-    CreatedAt: '2023-12-04T09:35:36.000Z',
-    Tag: 'Baekjoon',
-    ProgrammingLanguage: 'C++',
-    IsAdopted: false,
-    ViewCount: 0,
-    LikeCount: 0,
-  },
-  {
-    Id: 73761,
-    Title: '다익스트라 알고리즘의 정당성이 궁금해요',
-    CreatedAt: '2023-12-04T09:35:36.000Z',
-    Tag: 'Baekjoon',
-    ProgrammingLanguage: 'C++',
-    IsAdopted: false,
-    ViewCount: 18,
-    LikeCount: 0,
-  },
-  {
-    Id: 73760,
-    Title: '다익스트라 알고리즘의 정당성이 궁금해요',
-    CreatedAt: '2023-12-04T09:35:36.000Z',
-    Tag: 'Baekjoon',
-    ProgrammingLanguage: 'C++',
-    IsAdopted: false,
-    ViewCount: 0,
-    LikeCount: 0,
-  },
-];
 
-export function ProfileQuestionList() {
+interface QuestionListProps {
+  userQuestionList: Array<QuestionList>;
+}
+
+export function ProfileQuestionList({ userQuestionList }: QuestionListProps) {
   return (
     <Section>
       <Topic>나의 질문 </Topic>
@@ -51,16 +24,17 @@ export function ProfileQuestionList() {
             <p key={idx}>{val}</p>
           ))}
         </HeaderDiv>
-        {dummyData.map((data) => (
-          <ContentDiv key={data.Id}>
-            <p>{data.Title}</p>
-            <p>{data.Tag}</p>
-            <p>{data.ProgrammingLanguage}</p>
-            <p>{data.IsAdopted ? 'O' : 'X'}</p>
-            <p>{data.LikeCount}</p>
-            <p>{getFormalizedDate(data.CreatedAt)}</p>
-          </ContentDiv>
-        ))}
+        {userQuestionList &&
+          userQuestionList.map((data: any) => (
+            <ContentDiv key={data.Id}>
+              <p>{data.Title}</p>
+              <p>{data.Tag}</p>
+              <p>{data.ProgrammingLanguage}</p>
+              <p>{data.IsAdopted ? 'O' : 'X'}</p>
+              <p>{data.LikeCount}</p>
+              <p>{getFormalizedDate(data.CreatedAt)}</p>
+            </ContentDiv>
+          ))}
       </QuestionListDiv>
     </Section>
   );

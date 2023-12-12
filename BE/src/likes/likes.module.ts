422fc0480c0de4b5575a3e84c0835a533f4349ee
Kim Tae Ho <kok8454@gmail.com>
2023-12-12 18:04:10 +0900

    refactor: module에 의존성 주입 제거
    req.user.Id로 user의 pk값을 알아낼 수 있으므로 users.service.ts에서 getIdByUserId 메서드를 삭제 했습니다.
    users.service.ts의 메서드를 더 이상 사용하지 않는 module에 한해서만 주입했던 의존성을 제거했습니다.

--- BE/src/likes/likes.module.ts
+++ BE/src/likes/likes.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { LikesController } from './likes.controller';
 import { LikesService } from './likes.service';
 import { PrismaService } from '../prisma.service';
-import { UsersService } from '../users/users.service';
 
 @Module({
   controllers: [LikesController],
-  providers: [LikesService, PrismaService, UsersService],
+  providers: [LikesService, PrismaService],
 })
 export class LikesModule {}
--- BE/src/questions/questions.module.ts
+++ BE/src/questions/questions.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { QuestionsController } from './questions.controller';
 import { QuestionsService } from './questions.service';
 import { PrismaService } from '../prisma.service';
-import { UsersService } from '../users/users.service';
 
 @Module({
   controllers: [QuestionsController],
-  providers: [QuestionsService, PrismaService, UsersService],
+  providers: [QuestionsService, PrismaService],
 })
 export class QuestionsModule {}

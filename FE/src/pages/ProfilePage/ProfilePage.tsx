b14e641eb8bc2362c934f7b5688f8822e34e1545
BaeJ1H021 <blueapple99@yonsei.ac.kr>
2023-12-12 21:36:06 +0900

    feat: 프로필 페이지 api 수정
    - useQuery로 서버 데이터 fetching
    - 기존 5개 api에서 user/me api로 수정

--- FE/src/pages/ProfilePage/ProfilePage.tsx
+++ FE/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,85 +1,47 @@
-import { useEffect, useState } from 'react';
 import { ProfilePageMetas } from '../../metas/metas';
-import { AnswerList, QuestionList } from 'src/types/type';
-import {
-  getUserAnswerListAPI,
-  getUserGradeAPI,
-  getUserNicknameAPI,
-  getUserPointAPI,
-  getUserQuestionListAPI,
-} from '../../api/profile';
+import { getUserProfileAPI } from '../../api/profile';
 import {
   ProfileAnswerList,
   ProfileInfo,
   ProfileQuestionList,
 } from '../../components';
 import { Container, Inner } from './ProfilePage.styles';
-
-// 아직 유저 정보를 어떻게 가져올지 정해지지 않음.
-// 그래서 유저 아이디 그냥 임의로 넣음.
-const dummyUserId = 'user1';
-const dummyUserIdNum = 1;
-
-interface UserData {
-  userPoint: number;
-  userNickname: string;
-  userGrade: string;
-  userQuestionList: Array<QuestionList>;
-  userAnswerList: Array<AnswerList>;
-}
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 
 export default function ProfilePage() {
-  const [data, setData] = useState<UserData>({
-    userPoint: 0,
-    userNickname: '',
-    userGrade: '',
-    userQuestionList: [],
-    userAnswerList: [],
-  });
-
-  const fetchData = async () => {
-    try {
-      const [
-        userPointResult,
-        userNicknameResult,
-        userGradeResult,
-        userQuestionListResult,
-        userAnswerListResult,
-      ] = await Promise.all([
-        getUserPointAPI(dummyUserId),
-        getUserNicknameAPI(dummyUserId),
-        getUserGradeAPI(dummyUserId),
-        getUserQuestionListAPI(dummyUserIdNum),
-        getUserAnswerListAPI(dummyUserIdNum),
-      ]);
-      setData({
-        userPoint: userPointResult,
-        userNickname: userNicknameResult,
-        userGrade: userGradeResult,
-        userQuestionList: userQuestionListResult,
-        userAnswerList: userAnswerListResult,
-      });
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+  const getUserProfileData = async () => {
+    return await getUserProfileAPI();
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const { data: userProfileData } = useQuery({
+    queryKey: ['userProfile'],
+    queryFn: getUserProfileData,
+    staleTime: 10 * 1000,
+    gcTime: 30 * 1000,
+    placeholderData: keepPreviousData,
+  });
 
   return (
     <Container>
-      <ProfilePageMetas userName={data.userNickname} />
-      <Inner>
-        <ProfileInfo
-          userPoint={data.userPoint}
-          userNickname={data.userNickname}
-          userGrade={data.userGrade}
-        />
-        <ProfileQuestionList userQuestionList={data.userQuestionList} />
-        <ProfileAnswerList userAnswerList={data.userAnswerList} />
-      </Inner>
+      {userProfileData && (
+        <>
+          <ProfilePageMetas userName={userProfileData.nickname} />
+          <Inner>
+            <ProfileInfo
+              userPoint={userProfileData.points}
+              userNickname={userProfileData.nickname}
+              userGrade={userProfileData.grade}
+              userAdoptedAnswerCount={userProfileData.adoptedAnswerCount}
+              userAnswerCount={userProfileData.answerCount}
+              userQuestionCount={userProfileData.questionCount}
+            />
+            <ProfileQuestionList
+              userQuestionList={userProfileData.recentQuestions}
+            />
+            <ProfileAnswerList userAnswerList={userProfileData.recentAnswers} />
+          </Inner>
+        </>
+      )}
     </Container>
   );
 }

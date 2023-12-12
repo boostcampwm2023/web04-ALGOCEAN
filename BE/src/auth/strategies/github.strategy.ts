9c3336adc0fdf3cf6bb5adf89e2d76d2d626f31a
MayOwall <pixartive@gmail.com>
2023-12-12 12:50:54 +0900

    fix: github callback url 수정

--- BE/src/auth/strategies/github.strategy.ts
+++ BE/src/auth/strategies/github.strategy.ts
@@ -14,7 +14,7 @@ export class GithubStrategy extends PassportStrategy(Strategy, 'github') {
     super({
       clientID: configService.get('GITHUB_CLIENT_ID'),
       clientSecret: configService.get('GITHUB_CLIENT_SECRET'),
-      callbackURL: 'https://www.algocean.site/api/auth/github/callback',
+      callbackURL: '/login/githubcallback',
       scope: ['read:user'],
     });
   }

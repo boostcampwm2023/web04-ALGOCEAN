98f18657c7d5591ee6fb900a59e501504abcf554
Kim Tae Ho <kok8454@gmail.com>
2023-12-14 02:37:48 +0900

    feat: redis module 의존성 주입
    - env 파일을 활용하여 redis config 설정

--- sse/src/app.module.ts
+++ sse/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlarmModule } from './alarm/alarm.module';
+import { RedisModule } from '@nestjs-modules/ioredis';
 
 @Module({
-  imports: [AlarmModule],
+  imports: [
+    AlarmModule,
+    RedisModule.forRootAsync({
+      useFactory: () => ({
+        config: {
+          url: process.env.REDIS_URL,
+          password: process.env.REDIS_PASSWORD,
+          connectTimeout: 10000,
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

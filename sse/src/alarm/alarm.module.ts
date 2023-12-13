a1319550d3895a274582262466ad38cf2c6c93a6
Kim Tae Ho <kok8454@gmail.com>
2023-12-14 02:38:38 +0900

    feat: prisma service 의존성 주입
    - alarm module에 prisma service 주입

--- sse/src/alarm/alarm.module.ts
+++ sse/src/alarm/alarm.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AlarmController } from './alarm.controller';
 import { AlarmService } from './alarm.service';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [AlarmController],
-  providers: [AlarmService]
+  providers: [AlarmService, PrismaService],
 })
 export class AlarmModule {}

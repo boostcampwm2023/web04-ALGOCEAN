0d39a92e8a31fcbe471acf955a64175f4d620f87
Kim Tae Ho <kok8454@gmail.com>
2023-12-14 04:22:33 +0900

    feat: SSE 기능 구현
    - HOHO 채널을 감시하다가 HOHO 채널에 메세지가 도착하면 메세지에서 userId를 보고 해당 userId의 채널로 메세지를 전송함
    - Observable 객체가 userId에 해당하는 채널에 message 이벤트가 발생하면 이를 감지하고 SSE를 통해 client에게 data를 전송함
    - 전송 후 해당 알림을 읽음 처리 함

--- sse/src/alarm/alarm.controller.ts
+++ sse/src/alarm/alarm.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Req, Sse } from '@nestjs/common';
+import { Request } from 'express';
+import { AlarmService } from './alarm.service';
+import { InjectRedis, Redis } from '@nestjs-modules/ioredis';
 
 @Controller('alarm')
-export class AlarmController {}
+export class AlarmController {
+  constructor(
+    private readonly alarmService: AlarmService,
+    @InjectRedis() private readonly redis: Redis,
+  ) {}
+
+  @Sse(':userId')
+  sse(@Param('userId') userId: number, @Req() req: Request): any {
+    const userSubject = this.alarmService.createSseStreamForUser(userId);
+
+    req.on('close', () => this.alarmService.removeSseStreamForUser(userId));
+    return userSubject;
+  }
+}
